b46ddb2e2ba8c098ec2eaaa4a40f73008d8807d6
Ehigboria Dukeson O <studysampul@gmail.com>
2023-06-01 13:01:51 +0100

    Solution to Task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,9 +8,11 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (1);
-	else if (!node->left && !node->right)
-		return (1);
+	if (node)
+	{
+		if (!node->left && !node->right)
+			return (1);
+		return (0);
+	}
 	return (0);
 }
